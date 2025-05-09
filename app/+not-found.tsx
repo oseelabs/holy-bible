9bac0051ca34e8b392a98f5aed2b1c82b92be1f2
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2025-05-09 17:33:26 +0300

    Add eas

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,11 +1,22 @@
 import { Stack } from "expo-router";
 import React from "react";
+import {Text, View} from "react-native";
 
 
 export default function NotFound() {
 	return (
 	  <>
-		  <Stack.Screen title={"Oop!s"} />
+		  <Stack.Screen
+		    options={{
+			  title: "Oops!",
+			  headerShown: false,
+		    }}
+		  />
+		  <View>
+			  <Text>
+				  This isn't the page you're looking for...
+			  </Text>
+		  </View>
 	  </>
 	)
 }
